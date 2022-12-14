a9914497495c6278df38c58d8a163f0b77a5f4c7
DucNguyen <nguyenngocduc94751@gmail.com>
2022-12-14 21:14:43 +0700

    duc update slider

--- src/Components/Home/IntroductionHomePage/Slider/SliderData.js
+++ src/Components/Home/IntroductionHomePage/Slider/SliderData.js
@@ -1,13 +1,19 @@
 import slider3 from "../../../../assets/home/IntroductionHomePage/SliderIntroduction/slider3.jpg";
 import slider4 from "../../../../assets/home/IntroductionHomePage/SliderIntroduction/slider4.jpg";
+import slider5 from "../../../../assets/home/IntroductionHomePage/SliderIntroduction/1_VEGETABLES_2_VEGETABLES_4.gif";
 
 const dataSlides = [
   {
     id: 1,
-    imgSlider: slider4,
+    imgSlider: slider5,
   },
   {
-    id: 1,
+    id: 2,
+    imgSlider: slider4,
+  },
+
+  {
+    id: 3,
     imgSlider: slider3,
   },
 ];
